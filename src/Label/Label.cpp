cb726453766f8660dfd6f7b1dbc301918457f39f
gammasoft71 <gammasoft71@gmail.com>
2018-06-05 17:25:19 +0200

    add Label

---
+++ src/Label/Label.cpp
@@ -0,0 +1,28 @@
+#include <QApplication>
+#include <QMainWindow>
+#include <QLabel>
+
+class Form : public QMainWindow {
+  //Q_OBJECT
+
+public:
+  explicit Form(QWidget* parent = 0) : QMainWindow(parent) {
+    this->label1.setText("label1");
+    this->label1.move(10, 10);
+
+    this->setCentralWidget(&this->centralWidget);
+    this->setWindowTitle("Label example");
+    this->resize(300, 300);
+  }
+
+private:
+  QWidget centralWidget;
+  QLabel label1 {&this->centralWidget};
+};
+
+int main(int argc, char *argv[]) {
+  QApplication application(argc, argv);
+  Form form;
+  form.show();
+  return application.exec();
+}
---
+++ src/Label/Label.pro
@@ -0,0 +1,13 @@
+QT += core gui widgets
+
+TARGET = Label
+TEMPLATE = app
+
+DEFINES += QT_DEPRECATED_WARNINGS
+
+SOURCES += \
+    Label.cpp
+
+HEADERS +=
+
+FORMS =
