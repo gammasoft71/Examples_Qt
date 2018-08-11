c2ad0792e490c82d4e245e7e0f4646a436de24b5
Gammasoft71 <gammasoft71@gmail.com>
2018-08-11 14:25:06 +0200

    add CheckBox

--- src/HelloWorld/HelloWorld.cpp
+++ src/HelloWorld/HelloWorld.cpp
@@ -9,7 +9,7 @@ public:
     QPalette palette;
     palette.setColor(QPalette::WindowText, Qt::darkGreen);
     this->label1.setPalette(palette);
-    this->label1.setFont(QFont("Arial", 45, QFont::Bold, true));
+    this->label1.setFont(QFont("Arial", 34, QFont::Bold, true));
     this->label1.move(5, 100);
     this->resize(290, 100);
 
