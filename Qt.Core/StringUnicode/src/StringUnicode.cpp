36f354b4a16a59c6b8a255e3e25ef0b94a546c98
gammasoft71 <gammasoft71@gmail.com>
2019-03-13 11:18:44 +0100

    remove warnings

--- Qt.Core/StringUnicode/src/StringUnicode.cpp
+++ Qt.Core/StringUnicode/src/StringUnicode.cpp
@@ -2,17 +2,17 @@
 #include <QTextStream>
 
 int main() {
-  QString arabic = "\u0627\u0644\u0643\u0644\u0645\u0629 \u0627\u0644\u0639\u0631\u0628\u064A\u0629";
-  QString tradChinese = "\u4E2D\u570B\u7684\u6F22\u5B57";
-  QString simplChinese = "\u6C49\u8BED";
-  QString french = "Langue fran\u00E7aise";
-  QString greek = "\u0395\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AE \u03B3\u03BB\u03CE\u03C3\u03C3\u03B1";
-  QString hebrew = "\u05DB\u05EA\u05D1 \u05E2\u05D1\u05E8\u05D9\u05EA";
-  QString hindi = "\u0939\u093f\u0928\u094d\u0926\u0940 \u092d\u093e\u0937\u093e";
-  QString icelandic = "\u00CDslenska";
-  QString japanese = "\u65E5\u672C\u8A9E\u306E\u3072\u3089\u304C\u306A, \u6F22\u5B57\u3068\u30AB\u30BF\u30AB\u30CA";
-  QString korean = "\uB300\uD55C\uBBFC\uAD6D\uC758 \uD55C\uAE00";
-  QString russian = "\u0420\u0443\u0441\u0441\u043A\u0438\u0439 \u044F\u0437\u044B\u043A";
+  QString arabic = u8"\u0627\u0644\u0643\u0644\u0645\u0629 \u0627\u0644\u0639\u0631\u0628\u064A\u0629";
+  QString tradChinese = u8"\u4E2D\u570B\u7684\u6F22\u5B57";
+  QString simplChinese = u8"\u6C49\u8BED";
+  QString french = u8"Langue fran\u00E7aise";
+  QString greek = u8"\u0395\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AE \u03B3\u03BB\u03CE\u03C3\u03C3\u03B1";
+  QString hebrew = u8"\u05DB\u05EA\u05D1 \u05E2\u05D1\u05E8\u05D9\u05EA";
+  QString hindi = u8"\u0939\u093f\u0928\u094d\u0926\u0940 \u092d\u093e\u0937\u093e";
+  QString icelandic = u8"\u00CDslenska";
+  QString japanese = u8"\u65E5\u672C\u8A9E\u306E\u3072\u3089\u304C\u306A, \u6F22\u5B57\u3068\u30AB\u30BF\u30AB\u30CA";
+  QString korean = u8"\uB300\uD55C\uBBFC\uAD6D\uC758 \uD55C\uAE00";
+  QString russian = u8"\u0420\u0443\u0441\u0441\u043A\u0438\u0439 \u044F\u0437\u044B\u043A";
 
   QTextStream out(stdout);
   out << QString("Arabic:         %1").arg(arabic) << endl;
