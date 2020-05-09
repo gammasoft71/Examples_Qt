35d923546756acab01eb1e3aef4542dba9affb78
Gammasoft71 <gammasoft71@gmail.com>
2020-05-10 00:02:06 +0200

    rename to window1

--- Qt.Widgets/Containers/Window/src/Window.cpp
+++ Qt.Widgets/Containers/Window/src/Window.cpp
@@ -5,7 +5,7 @@ using namespace examples;
 
 int main(int argc, char *argv[]) {
   QApplication application(argc, argv);
-  Window1 form;
-  form.show();
+  Window1 window1;
+  window1.show();
   return application.exec();
 }
--- Qt.Widgets/FontDialog/src/FontDialog.cpp
+++ Qt.Widgets/FontDialog/src/FontDialog.cpp
@@ -46,7 +46,7 @@ private:
 
 int main(int argc, char *argv[]) {
   QApplication application(argc, argv);
-  Window1 form;
-  form.show();
+  Window1 window1;
+  window1.show();
   return application.exec();
 }
