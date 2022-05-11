eff768e143a8bd03540575af6ce92be1ca7aa643
Gammasoft <gammasoft71@gmail.com>
2022-05-11 21:48:02 +0200

    Update ToolBar.h

--- Qt.Widgets/MenusAndToolbars/ToolBar/src/ToolBar.h
+++ Qt.Widgets/MenusAndToolbars/ToolBar/src/ToolBar.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <QComboBox>
 #include <QDebug>
 #include <QFrame>
 #include <QMainWindow>
@@ -11,10 +12,18 @@ namespace Examples {
     Q_OBJECT
   public:
     Window1() {
+      stretchableSeparator.setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Ignored);
+
+      comboBox.addItems({"Red", "Green", "Blue", "Yellow"});
+
       toolBar.addAction(&newAction);
       toolBar.addAction(&openAction);
       toolBar.addAction(&saveAction);
-      toolBar.setAutoFillBackground(true);
+      toolBar.addSeparator();
+      toolBar.addAction(&printAction);
+      toolBar.addWidget(&stretchableSeparator);
+      toolBar.addWidget(&comboBox);
+      toolBar.setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
 
       connect(&newAction, &QAction::triggered, [&] {
         qDebug() << "ToolBar/New";
@@ -28,9 +37,23 @@ namespace Examples {
         qDebug() << "ToolBar/Save";
       });
 
+      connect(&printAction, &QAction::triggered, [&] {
+        qDebug() << "ToolBar/Print";
+      });
+
+      connect(&comboBox, &QComboBox::currentIndexChanged, [&] {
+        qDebug() << comboBox.currentText();
+      });
+
+      toolBar2.addAction(&trashAction);
+      toolBar2.setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+
+      addToolBar(&toolBar);
+      addToolBar(&toolBar2);
       setCentralWidget(&frame);
+      setUnifiedTitleAndToolBarOnMac(true);
       setWindowTitle("Tool bar example");
-      resize(300, 300);
+      resize(640, 480);
     }
 
   private:
@@ -38,6 +61,11 @@ namespace Examples {
     QToolBar toolBar {&frame};
     QAction newAction {style()->standardIcon(QStyle::StandardPixmap::SP_FileIcon), "&New", &toolBar};
     QAction openAction  {style()->standardIcon(QStyle::StandardPixmap::SP_DirOpenIcon), "&Open", &toolBar};
-    QAction saveAction {style()->standardIcon(QStyle::StandardPixmap::SP_DialogSaveButton), "&Open", &toolBar};
+    QAction saveAction {style()->standardIcon(QStyle::StandardPixmap::SP_DialogSaveButton), "&Save", &toolBar};
+    QAction printAction {"&Print", &toolBar};
+    QWidget stretchableSeparator {&frame};
+    QComboBox comboBox {&frame};
+    QToolBar toolBar2 {&frame};
+    QAction trashAction {style()->standardIcon(QStyle::StandardPixmap::SP_TrashIcon), "&Trash", &toolBar2};
   };
 }
